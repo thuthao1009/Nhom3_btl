3cb463a1368eede81e668c6aa8e383a2316c7d24
lethuonght <lethuong02.hvnh@gmail.com>
2022-10-19 13:47:50 +0700

    update 19/10

--- admin/quan_li_chung.php
+++ admin/quan_li_chung.php
@@ -108,7 +108,7 @@
                             </a>
                             <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
                                 <div class="sb-nav-link-icon"><i class="fas fa-shopping-cart"></i></div>
-                                Quản trị bán hàng
+                                Quản trị bán hàng hóa
                                 <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                             </a>
                             <div class="collapse" id="collapseThree" aria-labelledby="headingThree" data-bs-parent="#sidenavAccordion">
