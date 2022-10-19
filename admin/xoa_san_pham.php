4120c43964f622a475026a0482714369051c4a95
lethuonght <115983812+lethuonght@users.noreply.github.com>
2022-10-19 13:58:09 +0700

    Add files via upload

---
+++ admin/xoa_san_pham.php
@@ -0,0 +1,19 @@
+<?php
+	$id=$_GET["id"];
+
+	// Kết nối đến CSDL
+	require('../config.php');
+
+	// Truy vấn đến bảng dữ liệu
+	$sql="DELETE FROM sanpham WHERE sp_id=".$id;
+
+	// Thực hiện xoá dữ liệu
+	mysqli_query($con, $sql);
+echo ($sql);exit();
+	// Thực hiện thông báo cho mọi người biết các bạn xoá thành công
+;?>
+<script type="text/javascript">
+	window.alert("Xoá dữ liệu thành công!");
+	window.location.href = "../Admin/quan_tri_san_pham.php";
+</script>
+
