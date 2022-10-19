27c35116b90cefa589fc2695c28eec2d5a8bb385
lethuonght <115983812+lethuonght@users.noreply.github.com>
2022-10-19 10:36:28 +0700

    Add files via upload
    test

--- config.php
+++ config.php
@@ -1,13 +1,16 @@
-<?php
-		// Thông số kết nối đến máy chủ dữ liệu
-		$host="localhost";
-		$username="root";
-		$password="";
-		$dbname="databasewebba";
-
-		// Kết nối và kiểm tra xem có kết nối thành công đến máy chủ CSDL hay không?
-		$con=mysqli_connect($host, $username, $password, $dbname);
-		if (!$con) {
-			echo "Kết nối đến CSDL thất bại";
-		}
-;?>
+<?php 
+	$host="localhost";
+	$username="root";
+	$passwword="";
+	$dbname="webba";
+	$con = mysqli_connect($host,$username,$passwword,$dbname);
+	if ($con)
+	{
+		//echo "Kết nối đến CSDL thành công";
+	
+	}
+	else 
+	{
+		echo"thất bại";
+	}
+	;?>
