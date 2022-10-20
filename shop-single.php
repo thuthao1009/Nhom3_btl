1601418c231791f2997502a3e06ebaaed3929177
thuthao1009 <thuthao021009@gmail.com>
2022-10-20 16:24:01 +0700

    shop-single

--- shop-single.php
+++ shop-single.php
@@ -35,108 +35,48 @@
     $sql_lq ="SELECT * FROM sanpham WHERE sp_id<>'".$id."' AND tl_id LIKE (SELECT tl_id FROM sanpham WHERE sp_id='".$id."') ORDER BY `sanpham`.`created_at` DESC ";
         // echo $sql_lq; exit();
     $lien_quan=mysqli_query($con,$sql_lq);
+    $sql_anh="SELECT * FROM `thumucanh` WHERE sp_id='".$id."' ORDER BY `thumucanh`.`tma_id` ASC ";
+    $anh=mysqli_query($con,$sql_anh);
+    $row_anh=mysqli_fetch_array($anh);
+    $sql_sl_anh="SELECT COUNT(*) FROM `thumucanh` WHERE sp_id='".$id."' ORDER BY `thumucanh`.`tma_id` ASC ";
+    $sl_anh=mysqli_query($con,$sql_sl_anh);
+    $row_sl_anh=mysqli_fetch_array($sl_anh);
+
+
+
     ?>
+
+    <!-- Start Article -->
+ <!-- Start Article -->
+ 
+    <!-- End Article -->
+
     <!-- Open Content -->
     <section class="bg-light">
         <div class="container pb-5">
             <div class="row" >
-                <div class="col-lg-5 mt-5">
-                    <div class="card mb-3">
-                        <img class="card-img img-fluid" src="assets/img/product_single_10.jpg" alt="Card image cap" id="product-detail">
-                    </div>
-                    <div class="row">
-                        <!--Start Controls-->
-                        <div class="col-1 align-self-center">
-                            <a href="#multi-item-example" role="button" data-bs-slide="prev">
-                                <i class="text-dark fas fa-chevron-left"></i>
-                                <span class="sr-only">Previous</span>
-                            </a>
-                        </div>
-                        <!--End Controls-->
-                        <!--Start Carousel Wrapper-->
-                        <div id="multi-item-example" class="col-10 carousel slide carousel-multi-item" data-bs-ride="carousel">
-                            <!--Start Slides-->
-                            <div class="carousel-inner product-links-wap" role="listbox">
-
-                                <!--First slide-->
-                                <div class="carousel-item active">
-                                    <div class="row">
-                                        <div class="col-4">
-                                            <a href="#">
-                                                <img class="card-img img-fluid" src="assets/img/product_single_01.jpg" alt="Product Image 1">
-                                            </a>
-                                        </div>
-                                        <div class="col-4">
-                                            <a href="#">
-                                                <img class="card-img img-fluid" src="assets/img/product_single_02.jpg" alt="Product Image 2">
-                                            </a>
-                                        </div>
-                                        <div class="col-4">
-                                            <a href="#">
-                                                <img class="card-img img-fluid" src="assets/img/product_single_03.jpg" alt="Product Image 3">
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div>
-                                <!--/.First slide-->
-
-                                <!--Second slide-->
-                                <div class="carousel-item">
-                                    <div class="row">
-                                        <div class="col-4">
-                                            <a href="#">
-                                                <img class="card-img img-fluid" src="assets/img/product_single_04.jpg" alt="Product Image 4">
-                                            </a>
-                                        </div>
-                                        <div class="col-4">
-                                            <a href="#">
-                                                <img class="card-img img-fluid" src="assets/img/product_single_05.jpg" alt="Product Image 5">
-                                            </a>
-                                        </div>
-                                        <div class="col-4">
-                                            <a href="#">
-                                                <img class="card-img img-fluid" src="assets/img/product_single_06.jpg" alt="Product Image 6">
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div>
-                                <!--/.Second slide-->
-
-                                <!--Third slide-->
-                                <div class="carousel-item">
-                                    <div class="row">
-                                        <div class="col-4">
-                                            <a href="#">
-                                                <img class="card-img img-fluid" src="assets/img/product_single_07.jpg" alt="Product Image 7">
-                                            </a>
-                                        </div>
-                                        <div class="col-4">
-                                            <a href="#">
-                                                <img class="card-img img-fluid" src="assets/img/product_single_08.jpg" alt="Product Image 8">
-                                            </a>
-                                        </div>
-                                        <div class="col-4">
-                                            <a href="#">
-                                                <img class="card-img img-fluid" src="assets/img/product_single_09.jpg" alt="Product Image 9">
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div>
-                                <!--/.Third slide-->
+                <!-- slide ảnh -->
+                 <div class="col-lg-5 mt-5">
+                        <div class="container product-links-wap ">
+                            <div class="card  mb-3 " >  
+                                <img class="card-img img-fluid"  src="assets/img/<?php  echo $row_anh['anhminhhoa'] ?>" alt="Card image cap" id="product-detail" style="height: 550px;">
                             </div>
-                            <!--End Slides-->
+                            <br>
+                           
+
+                            <!--Start Carousel Wrapper-->
+                            <div id="carousel-image" >
+                                <?php for ($i=0; $row_anh=mysqli_fetch_array($anh) ; $i++) { ?>
+                                    <div class="col-3 ">
+                                        <a href="#" >
+                                            <img class="card-img img-fluid " onclick="currentSlide(<?php  echo $i ?>)" src="assets/img/<?php  echo $row_anh['anhminhhoa'] ?>" alt="Product Image 1">
+                                        </a>
+                                    </div>
+                                <?php }; ?>
+                            </div>
                         </div>
-                        <!--End Carousel Wrapper-->
-                        <!--Start Controls-->
-                        <div class="col-1 align-self-center">
-                            <a href="#multi-item-example" role="button" data-bs-slide="next">
-                                <i class="text-dark fas fa-chevron-right"></i>
-                                <span class="sr-only">Next</span>
-                            </a>
-                        </div>
-                        <!--End Controls-->
-                    </div>
                 </div>
+               <!--  slide ảnh -->
                 <!-- col end -->
                 <div class="col-lg-7 mt-5">
                     <div class="card">
@@ -170,7 +110,7 @@
                             </ul>
 
                             <h6>Mô tả</h6>
-                            <div class="content hideContent text-decoration-none" style="height: 260px;"><?php echo $row['sp_mo_ta']  ?></div>
+                            <div class="content hideContent text-decoration-none" style="height: 240px;"><?php echo $row['sp_mo_ta']  ?></div>
                             <ul class="list-inline">
                                 <li class="list-inline-item">
                                     <h6>Còn lại:</h6>
@@ -254,6 +194,7 @@
         </div>
     </section>
     <!-- End Article -->
+    <?php require('footer.php');?>
 
 
     <!-- Start Script -->
@@ -272,7 +213,38 @@
             arrows: false,
             slidesToShow: 4,
             slidesToScroll: 3,
-            dots: true,
+            dots: false,
+            responsive: [{
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 3
+                    }
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 3
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 3
+                    }
+                }
+            ]
+        });
+    </script>
+    <script>
+        $('#carousel-image').slick({
+            infinite: true,
+            arrows: false,
+            slidesToShow: 4,
+            slidesToScroll: 3,
+            dots: false,
             responsive: [{
                     breakpoint: 1024,
                     settings: {
