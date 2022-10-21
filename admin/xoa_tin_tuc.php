eb613458c8b8979250b61ea80d80957203db9b48
dinhthithuyquynh <116150565+dinhthithuyquynh@users.noreply.github.com>
2022-10-21 13:02:43 +0700

    Add files via upload

---
+++ admin/xoa_tin_tuc.php
@@ -0,0 +1,14 @@
+<?php
+     $id= $_GET['id'];
+     //kết nối đến csdl
+     require('../config.php');
+     //truy vấn đến bảng dữ liệu
+     $sql= "DELETE FROM tintuc WHERE tt_id=".$id;
+     //thực hiện xóa dữ liệu
+     mysqli_query($con,$sql);
+     //thông báo đã xóa tin tức
+;?>
+<script type="text/javascript">
+	window.alert("Xóa dữ liệu thành công!");
+	window.location.href="../admin/quan_tri_tin_tuc.php";
+</script>
